92605273407a332e716a253c7bd270038c962f45
Babuthetitan <landiasteve@gmail.com>
2023-03-22 23:19:59 +0000

    Connected Source control
    Connected source control to my learning C++ repository

---
+++ Hello.cpp
@@ -0,0 +1,26 @@
+// Hello.cpp  
+// This is me learning C++
+// I am extremely proud of myself for going up this learning curve
+// include <iostream> = a normal C++ library that means input and output stream
+// int main() is where one stores all the code they want to run with the more complx brackets at the end 
+// int = integer, which is a whole number
+// variable = used as a variable/placeholder for something
+// std = standard namespace C++ library
+// cout= standard output stream that prints data to the (console out)
+// float= used to declare decimal type numbers
+// double= similar to float with more precision 
+//string= a list of characters,
+
+#include <iostream>
+
+int main()
+{
+    std::string name = "Stephen Babu";
+    std::string position = "audio software engineer";
+    int a = 2;
+    int b = 10;
+ 
+    std::cout << "Name:" << name << std::endl;
+    std::cout << "position:" << position << std::endl;
+
+}
